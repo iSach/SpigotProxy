c3b640c10314b7dcd72468dcf7c8f8b9832ab4c5
iSach <sacha.lewin@me.com>
2019-08-24 17:54:49 +0200

    Dynamic support for > 1.14

--- src/main/java/nl/thijsalders/spigotproxy/SpigotProxy.java
+++ src/main/java/nl/thijsalders/spigotproxy/SpigotProxy.java
@@ -79,23 +79,14 @@ public class SpigotProxy extends JavaPlugin {
     public String getChannelFieldName(String version) {
         String name = null;
         switch (version) {
-            case "v1_14_R1":
-            case "v1_13_R2":
-            case "v1_13_R1":
-            case "v1_12_R1":
-            case "v1_11_R1":
-            case "v1_10_R1":
-            case "v1_9_R2":
-            case "v1_9_R1":
-            case "v1_8_R2":
-            case "v1_8_R3":
-                name = "g";
-                break;
             case "v1_8_R1":
                 name = "f";
                 break;
             case "v1_7_R4":
                 name = "e";
+                break;
+            default:
+                name = "g";
                 break;
         }
         return name;
